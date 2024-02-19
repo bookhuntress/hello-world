c9a3852944d95a44f3faedfc870186332bdc7d71
Butch Ellen Grace SALEM <butch.salem@thalesgroup.com>
2024-02-19 18:02:31 +0800

    added functionality to ask the user's name

--- src/com/sample/main/HelloWorld.java
+++ src/com/sample/main/HelloWorld.java
@@ -1,8 +1,33 @@
 package com.sample.main;
 
+import javax.swing.JOptionPane;
 
 public class HelloWorld {
+	public static final int MAX_ATTEMPTS = 3;
+	
 	public static void main(String[] args){
-		System.out.println("Hello, world!");
+		String userName = null;
+		int counter = 0;
+		boolean isOK = false;
+		
+		while(userName == null || userName.isEmpty() || counter < MAX_ATTEMPTS) {
+			if (counter >= MAX_ATTEMPTS) {
+				break;
+			}
+			counter++;
+			
+			userName = JOptionPane.showInputDialog(null, "Hello, world!\nHi, user!\nMay I know what is your name?");
+			if (userName != null && !userName.trim().isEmpty()) {
+				isOK = true;
+				break;
+			}
+		}
+		
+		if (!isOK) {
+			JOptionPane.showMessageDialog(null, "Input is invalid and max attempts have been reached.\nPlease try again next time.\nThank you and goodbye!", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Nice to meet you, " + userName + "!\nSee you next time!");
+		}
 	}
 }
