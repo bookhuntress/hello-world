9bcf4c7603cec298e4e682e121bdcb3f339c114a
Butch Ellen Grace SALEM <butch.salem@thalesgroup.com>
2024-02-19 17:12:22 +0800

    updated README to be more descriptive and modified HelloWorld to ask user to choose 1 out of 3 options and display his/her choice

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a test edit.
+This project was created to be able to experiment on Git functionality such as branch and merge. It can also be used for source code experiments.
--- src/com/sample/main/HelloWorld.java
+++ src/com/sample/main/HelloWorld.java
@@ -1,8 +1,14 @@
 package com.sample.main;
 
+import javax.swing.JOptionPane;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello, world!");
+		Object[] possibleValues = { "First", "Second", "Third" };
+		Object selectedValue = JOptionPane.showInputDialog(null,
+			"Choose one", "Input",
+			JOptionPane.INFORMATION_MESSAGE, null,
+			possibleValues, possibleValues[0]);
+		JOptionPane.showMessageDialog(null, "You selected: " + selectedValue);
 	}
 }
